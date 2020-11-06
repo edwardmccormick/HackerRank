11c3c529b58fc173327967a39f185451885f1e46
edwardmccormick <ted.mccormick@gmail.com>
2020-11-05 20:55:20 -0600

    First commit and first answer - woot!

---
+++ README.md
@@ -0,0 +1,3 @@
+# HackerRank Problems
+
+This is very similar to my Codeup exercise repository - just an opportunity for me to store the problems that I'm currently working on and also to archive the solutions I've found. 
---
+++ js/external.js
@@ -0,0 +1,11 @@
+"use strict";
+//https://www.hackerrank.com/challenges/compare-the-triplets/problem?h_r=next-challenge&h_v=zen
+//fit add
+// function compareTriplets(a, b) {
+//     var alice = 0; var bob = 0
+//     for (var i = 0; i < a.length; i++) {
+//         if (a[i]>b[i]) {alice++}; if (b[i]>a[i]) {bob++}
+//     }
+//     return [alice,bob];
+// }
+
